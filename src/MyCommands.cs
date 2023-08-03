86e276ac737adddc8fce6c35c1a09db0a575a7a3
Jowenn Lua <jowennl@gmail.com>
2023-08-04 09:01:58 +1000

    Initial project setup

---
+++ src/MyCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace MyTestC3DPlugin
+{
+    public class MyCommands
+    {
+        [CommandMethod("JL-TestCommand")]
+        public static void TestCommand()
+        {
+            Document adoc = AcadApp.DocumentManager.MdiActiveDocument;
+            Editor ed = adoc.Editor;
+
+          
+        }
+    }
+}
